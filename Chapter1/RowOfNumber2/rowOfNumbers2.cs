3d3af0480945ef23f2eaa16e22ed7c2ed93f275a
jorgeAML <martjorge8@gmail.com>
2021-12-15 15:08:16 -0600

    Row of numbers level 2

---
+++ Chapter1/RowOfNumber2/rowOfNumbers2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise2 
+{
+    class rowOfNumbers2
+    {
+        static void Main(String[] args)
+        {
+            int numberA = 47;
+            int numberB = 52;
+            int numberC = 150;
+            Console.WriteLine(numberA + "-" + "-" + numberB + "-" + "-" + numberC);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Algorithms-and-Exercises
+Exercises using C# while we make games and homeworks.
 ##  Chapter 1 - Input and Output from the console 
 
