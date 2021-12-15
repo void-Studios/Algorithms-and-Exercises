e1f512277bdc5f22a040f31c0724f68accc1efb2
JorgeAML <martjorge8@gmail.com>
2021-12-15 15:56:44 -0600

    Exercise 3

--- Chapter1/README.md
+++ Chapter1/README.md
@@ -1,9 +1,13 @@
 # Chapter 1
-## 1. A row of numbers
+### 1. A row of numbers
 Introduce in only one row the numbers 31, 18 and 79 with one space between them. You can not use '31 18 79'.
 
 ---
-## 2. A row of numbers level 2
+### 2. A row of numbers level 2
 Introduce in only one row the numbers 47, 52 and 150 with two spaces between them. You can not use '47  52  150'.
+
+---
+### 3. Reading numbers from the console
+Introduce from the console the numbers 50 and 10 separately and then use a conditional to exit the program if the user is in the right range.
 
 ---
---
+++ Chapter1/fromConsole1/FromConsole.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Exercise3
+{
+    class FromConsoleOne
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Introduce the numbers less than 100 but grather than 0: ");
+            string numberA = Console.ReadLine();
+            Console.WriteLine("Your second number: ");
+            string numberB = Console.ReadLine();
+            int numberConvertA = Convert.ToInt32(numberA);
+            int numberConvertB = Convert.ToInt32(numberB);
+            if (numberConvertA > 0 && numberConvertB <= 100 || numberConvertB > 0 && numberConvertA <= 100) {
+                Console.WriteLine("You are in the right range");
+            } else {
+                Console.WriteLine("Your numbers are wrong");
+            }
+        }
+    }
+}
