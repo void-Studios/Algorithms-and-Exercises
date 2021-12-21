06868704dfc040ccab74fa81cad1761fd63bfea2
root <root@debian>
2021-12-21 11:16:53 -0600

    Exercise 7

---
+++ Chapter1/Exercise7/Exercise7.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Chapter1
+{
+    class Exercise7
+    {
+        static void Main(String[] args)
+        {
+            double naturalLogaritm = Math.E;
+            Console.WriteLine("The Natural Logarithm number is: " + naturalLogaritm);
+            Console.WriteLine("To the nearest tenth is: {0:#.#}", naturalLogaritm);
+        }
+    }
+}
