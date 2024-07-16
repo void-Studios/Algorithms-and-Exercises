09da3cbe0ce7dd50a7600aeec004cda51972bb03
Jorge aml <martjorge8@gmail.com>
2024-07-15 21:25:13 -0600

    My First C

---
+++ c_programming/variables/variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main (void) {
+    float a;
+    float b = 3.641;
+    float c;
+
+    a = 2.897;
+    c = a + b;
+    printf("The sum of adding %.3f and %.3f is %.3f \n", a,b,c);
+}
