f8f8693a7df23281ddb21976c8275ac69139ef7e
Jorge aml <martjorge8@gmail.com>
2024-07-16 17:56:07 -0600

    switch and break

---
+++ c_programming/variables/leavingALoopEarly.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(void) {
+	int a = 0;
+	
+	while(1) {
+		printf("a is equal to %d\n",a);
+		a++;
+		if (a == 5) {
+			break;
+		}
+	}
+	printf("a is equal to %d and I have finished \n",a);
+}
