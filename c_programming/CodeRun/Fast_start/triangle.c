96f7cb849f0cead650d8e93e54c5fae3501c8062
Jorge aml <martjorge8@gmail.com>
2024-07-25 22:40:40 -0600

    Triangle 4/9 passed

---
+++ c_programming/CodeRun/Fast_start/triangle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	char input[256];
+	int a, b, c;
+	
+	
+	
+	fgets(input,256,stdin);
+	sscanf(input, "%d", &a);
+	
+	fgets(input,256,stdin);
+	sscanf(input, "%d", &b);
+	
+	fgets(input,256,stdin);
+	sscanf(input, "%d", &c);
+	
+	int sum1 = a + b;
+	int sum2 = a + c;
+	int sum3 = b + c;
+	
+	if (sum1 > c) {
+		if (sum2 > b) {
+			if (sum3 > a) {
+				printf("YES \n");
+			}
+		}
+	} else {
+		printf("NO");
+	}
+	return 0;
+}
