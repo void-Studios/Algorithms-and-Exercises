7f1324928bfa29620601c8378d0340da56e3bc74
jorgeAML <martjorge8@gmail.com>
2025-08-16 15:55:58 -0600

    Proyecto actualizado

--- Chapter5/Proyecto1_9.c
+++ Chapter5/Proyecto1_9.c
@@ -1,9 +1,37 @@
+/*
+ * 2025-08-04  jorge AML  <jorge@debian>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ */
 #include <stdio.h>
 
 int main(void){
 	//Writes a program that prompts the user to enter two dates and then indicates which date comes earlier on the calendar.
 	int day, month, year;
 	int day2,month2,year2;
+	printf("***|	  **\n");
+	printf("***|	**\n");
+	printf("***|  **\n");
+	printf("*******\n");
+	printf("*******	\n");
+	printf("*******\n");
+	printf("***|  **\n");
+	printf("***|	**\n");
+	printf("***|	  **\n");
+	printf("-----------KURAI STUDIOS SOFTWARE ------------\n");
 	printf("**** ---- ** Print your dates as: 5/8/2001 or 12/1/99 **** ---- ** \n");
 	printf("Enter first date (mm/dd/yy): ");
 	scanf("%d/%d/%d", &month,&day, &year);
@@ -22,17 +50,41 @@ int main(void){
 				printf("%d/%d/%d is earlier than %d/%d/%d year condition",month,day,year,month2,day2,year2 );
 				};
 			};
-		} else if(year > year2 || year2 < year){
+		} if(year > year2 || year2 < year){
 				if(month < month2 || month > month2 || month2 < month || month2 > month || month2 == month){
 					if(day < day2 || day > day2 || day2 < day || day2 > day || day == day2) {
 						printf("%d/%d/%d is earlier than %d/%d/%d year condition",month2,day2,year2,month,day,year);
 					};
 				};
 		//THE LOGIC OF DAYS IN KURAI STUDIOS
-		} else if(day < day2 || day2 > day) {
+		} if(day < day2 || day2 > day) {
 			if(month == month2 || month < month2) {
 				if(year == year2 || year < year2) {
 					printf("%d/%d/%d is earlier than %d/%d/%d Oh my god those days!",month,day,year,month2,day2,year2);
+				};
+			};
+		} if(day < day2 || day2 > day){
+			if(month == month2 || month < month2) {
+				if(year == year2 || year < year2) {
+					printf("%d/%d/%d is earlier than %d/%d/%d Oh my god those days!",month,day,year,month2,day2,year2);
+				};
+			};
+		} if(day > day2 || day2 < day) {
+			if(month == month2 || month2 < month) {
+				if(year == year2 || year2 < year) {
+					printf("%d/%d/%d is earlier than %d/%d/%d Oh my god those days!",month2,day2,year2,month,day,year);
+				};
+			};	
+		} if(month < month2 || month2 > month) {
+			if(day == day2 || day < day2 || day2 > day || day > day2 || day2> day) {
+				if(year == year2) {
+					printf("%d/%d/%d is earlier than %d/%d/%d month condition",month,day,year,month2,day2,year2);
+				};
+			};
+		} if(month > month2 || month2 < month) {
+			if(day == day2 || day < day2 || day2 > day) {
+				if(year == year2) {
+					printf("%d/%d/%d is earlier than %d/%d/%d month condition",month2,day2,year2,month,day,year);
 				};
 			};
 		}
@@ -52,7 +104,7 @@ int main(void){
 				printf("%d/%d/%d is earlier than %d/%d/%d year condition",month2,day2,year2,month,day,year);
 			};
 		};
-	};*/
+	};
 	//THE LOGIC OF DAYS IN KURAI STUDIOS
 	if(day < day2 || day2 > day){
 		if(month == month2 || month < month2) {
@@ -82,6 +134,6 @@ int main(void){
 				printf("%d/%d/%d is earlier than %d/%d/%d month condition",month2,day2,year2,month,day,year);
 			};
 		};
-	};
+	};*/
 	return 0;
 }
